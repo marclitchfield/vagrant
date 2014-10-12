1aae9d80412c585e57b355126e79b8b293526ea7
marclitchfield <marclitchfield@gmail.com>
2014-10-12 13:57:23 -0700

    Configure npm so you can do 'npm install -g' without sudo

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -3,6 +3,7 @@
 apt-get install software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
+
 apt-get install -y git
 
 apt-get install -y docker.io
@@ -11,6 +12,10 @@ sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 docker pull ubuntu
 
 apt-get install -y ansible 
+
 apt-get install -y nodejs
 ln -sf /usr/bin/nodejs /usr/local/bin/node
+
 apt-get install -y npm
+npm config set prefix ~/npm
+echo 'export PATH="$PATH:$HOME/npm/bin"' >> /home/vagrant/.bashrc
