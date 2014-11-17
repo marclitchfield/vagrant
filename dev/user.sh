a2fe2d455ee60afaeaab07f8417eb5762aca4cfe
Marc Litchfield <marclitchfield@gmail.com>
2014-11-17 06:50:29 +0000

    Working zero-downtime deployment

--- dev/user.sh
+++ dev/user.sh
@@ -1,7 +1,11 @@
+# ssh
+ssh-keygen
+eval `ssh-agent -s`
+ssh-add
+
 # setup code
 cd /home/vagrant/thedebate-web
 npm install
 bower install
 mkdir node_modules/protractor/selenium
 webdriver-manager update --out_dir=node_modules/protractor/selenium
-
